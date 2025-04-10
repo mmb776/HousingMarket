0b7bd9b71e9a2f4bbafa2a78e6933447e10c4f7a
ShaNiece <157452727+STwittyDataLab@users.noreply.github.com>
2025-04-10 15:25:07 -0700

    Add files via upload

---
+++ docs/WhitevSaletoList.R
@@ -0,0 +1,40 @@
+library(HousingMarket)
+library(tidyverse)
+library(paletteer)
+library(viridis)
+library(paletteer)
+HousingMarket$Month.of.Period.End <- ym(HousingMarket$Month.of.Period.End)
+
+
+WhiteInteraction <-lm(Average.Sale.To.List ~`PopPct_WHITE ALONE`*Region, data = HousingMarket)
+#summary(WhiteInteraction)
+#plot(lm4)
+anova(WhiteInteraction)
+
+HousingMarket <- HousingMarket %>%
+  dplyr::select( -matches('fit'), -matches('lwr'), -matches('upr') ) %>%
+  cbind( predict(WhiteInteraction, newdata=., interval='confidence') )
+
+ggplot(HousingMarket , aes(x = `PopPct_WHITE ALONE` , y = Average.Sale.To.List , color = Region)) +
+  
+  geom_point(shape = 1,size = 2, alpha = 0.4) +
+  geom_line( aes(y=fit),alpha = 1,linewidth = 1.5 ) +
+  scale_color_viridis_d(option = "C",begin =0.1, end = 0.9)+
+  labs(
+    title = "Housing sale Discrepancy",
+    x = "Percent of White Population",
+    y = "Average Close Price to List Price Ratio",
+    color = "Region"
+  ) +
+  geom_hline(yintercept = 100, linetype = "dashed", color = "black", size = .5) +
+  #scale_x_date(date_labels = " %Y", date_breaks = "1 year")+
+  theme_classic()+
+  theme(plot.title = element_text(size = 15,hjust = 0.5, face = "bold"),
+        axis.text.x = element_text(size = 15, face = "bold"),
+        axis.text.y = element_text( size = 15, face = "bold"),
+        axis.title = element_text(size = 15, face = "bold"),
+        legend.text = element_text(size = 15),
+        legend.title = element_text(size = 15, face = "bold"),
+        legend.position = c(0.8, 0.8),
+        legend.background = element_blank(),
+        legend.box.background = element_rect(colour = "black"))
